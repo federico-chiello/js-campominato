19fbfa758c2372c67c9cfeacb9c9a34746b196e4
Fede <chiellofederico@gmail.com>
2020-12-22 17:54:10 +0100

    correzione

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // (1) Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
 // (2) In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
-// (3) Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // (4) Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
@@ -38,18 +38,16 @@ while (numeriGiocatore.length < 5) {
   } else {
     alert('Inserisci un numero diverso da quelli già inseriti:');
   }
+
+  var partita;
+
+  if (numeriGiocatore[numeroUtente] != numeriRandomCpu[numeroComputer]) {
+    partita = parseInt(prompt('Inserisci un numero:'));
+  } else if (numeriGiocatore[numeroUtente] == numeriRandomCpu[numeroComputer]){
+    partita = 'partita terminata';
+  }
 }
 console.log(numeriGiocatore);
-
-// (3) Terzo Step
-
-var partita;
-
-if (numeriGiocatore[numeroUtente] != numeriRandomCpu[numeroComputer]) {
-  partita = parseInt(prompt('Inserisci un numero:'));
-} else if (numeriGiocatore[numeroUtente] == numeriRandomCpu[numeroComputer]){
-  partita = 'partita terminata';
-}
 
 console.log(partita);
 
